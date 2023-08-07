27fdcc39f3c8ad877c64e1850c06e6abac8ec72a
vicperez208 <84733140+vicperez208@users.noreply.github.com>
2023-08-07 00:39:09 -0400

    Update generate.cpp

--- generate.cpp
+++ generate.cpp
@@ -1,10 +1,4 @@
-// Victor Perez
-// CECS 325-02
-// Prog 3 – Sorting Contest
-// 03/07/23
-//
-// I certify that this program is my own original work. I did not copy any part of this program from
- // any other source. I further certify that I typed each and every line of code in this program
+// generates a file containing random numbers within given range. 
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
