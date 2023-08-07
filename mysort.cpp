d428a48ad6b20b18a60095172ef17484801121fd
vicperez208 <84733140+vicperez208@users.noreply.github.com>
2023-08-07 00:36:33 -0400

    Update mysort.cpp

--- mysort.cpp
+++ mysort.cpp
@@ -1,10 +1,4 @@
-// Victor Perez
-// CECS 325-02
-// Prog 3 – Sorting Contest pthread
-// 03/21/23
-//
-// I certify that this program is my own original work. I did not copy any part of this program from
- // any other source. I further certify that I typed each and every line of code in this program
+// optimizing bubble sort using pThreads and then merging sub arrays to one big array to an ouput file.  
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
